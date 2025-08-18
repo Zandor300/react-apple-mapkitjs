31092e64b8e9959211c2a34318bd81d6d2c44ad8
Zandor Smith <info@zsinfo.nl>
2025-08-18 19:29:25 +0200

    feat: Add ColorScheme.Adaptive

--- src/lib/ColorScheme.js
+++ src/lib/ColorScheme.js
@@ -1,10 +1,12 @@
 const Light = "light"
 const Dark = "dark"
+const Adaptive = "adaptive"
 
 const isOneOf = (value) => {
     switch(value) {
         case Light:
         case Dark:
+        case Adaptive:
             return true
         default:
             return false
@@ -14,5 +16,6 @@ const isOneOf = (value) => {
 export default {
     Light,
     Dark,
+    Adaptive,
     isOneOf
 }
